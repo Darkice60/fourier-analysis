57f9750b46f2ee25cdae1a9ede87d4f01b2571ff
DarkIce60 <167371131+Darkice60@users.noreply.github.com>
2026-09-20 14:40:30 -0400

    attempted fourier transform numerically

--- python_files/main.py
+++ python_files/main.py
@@ -1,3 +1,29 @@
 # Darkice60
 # main file for fourier-analysis
 
+import math
+import cmath
+
+def fourier_transform(f, xi, t, n):
+    delta_t = (2*t)/(n-1)
+    value = 0j
+    
+    for i in range(math.ceil(n)):
+        x = -t + i * delta_t
+        value += f(x) * cmath.exp(-1j * 2 * math.pi * xi * x) * delta_t
+    return value
+
+def create_func(func_string):
+    def f(x):
+        func = eval(func_string, {"x": x, "math": math, "cmath": cmath})
+        return func
+    return f
+
+while True:
+    func_string = input("Enter your function. \"USE CMATH FOR FUNCTIONS\"\n")
+    func = create_func(func_string)
+    xi = float(input("xi value?\n"))
+    t = float(input("inetgration domain? \"(make it so that the function decays to zero fast enough so that the domain to infinity is insignificant)\"\n"))
+    n = float(input("number of steps?\n"))
+    f_hat = fourier_transform(func, xi, t, n)
+    print(f_hat)
