3401c939dc7626bbc3717700ce5f7ff3fc65971a
DarkIce60 <167371131+Darkice60@users.noreply.github.com>
2026-09-22 21:54:40 -0400

    added coarse frequency finder

--- python_files/main.py
+++ python_files/main.py
@@ -20,6 +20,21 @@ def create_func(func_string):
         return func
     return f
 
+def find_high(xi_min, xi_max):
+    results = []
+    peaks = []
+    for xi in np.arange(xi_min, xi_max, 0.05):
+                f_hat = fourier_transform(func, xi, t, n)
+                results.append((xi, f_hat, abs(f_hat)))
+    for i in range(1, len(results) -1):
+        _, _, magnitude_before = results[i - 1]
+        xi, f_hat, magnitude = results[i]
+        _, _, magnitude_after = results[i + 1]
+        if magnitude_before < magnitude and magnitude > magnitude_after:
+            peaks.append((xi, f_hat, magnitude))
+    return peaks
+
+
 choice = 2
 dest = False
 
@@ -28,18 +43,15 @@ while not dest:
     func = create_func(func_string)
     if choice == 1:
         xi = float(input("xi value?\n"))
-        t = float(input("inetgration domain? \"(make it so that the function decays to zero fast enough so that the domain to infinity is insignificant)\"\n"))
+        t = float(input("integration domain? \"(make it so that the function decays to zero fast enough so that the domain to infinity is insignificant)\"\n"))
         n = float(input("number of steps?\n"))
         f_hat = fourier_transform(func, xi, t, n)
         print(f_hat)
     elif choice == 2:
-        results = []
-        t = float(input("inetgration domain? \"(make it so that the function decays to zero fast enough so that the domain to infinity is insignificant)\"\n"))
+        t = float(input("integration domain?\n"))
         n = float(input("number of steps?\n"))
-        xi_min = float(input("What is the lowest frequency you would like to search for?"))
-        xi_max = float(input("What is the highest frequency you would like to search for?"))
-        res = float(input(""))
-        for xi in np.arange(xi_min, xi_max, res):
-            f_hat = fourier_transform(func, xi, t, n)
-            print(f_hat)
-    dest = bool(input("End?"))
+        xi_min = float(input("what is the lowest frequency you would like to search for?\n"))
+        xi_max = float(input("what is the highest frequency you would like to search for?\n"))
+        peaks = find_high(xi_min, xi_max)
+
+    dest = bool(input("End?\n"))
